d5690354576da09e96e5011a2cefbcd584ca4ebd
Charles Eromose <okuekhamhene.gmail.com>
2025-06-03 07:14:58 +0100

    complete styling of nav and home

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+
+
+var typed = new Typed('.typing', {
+  strings: ['Back-end Developer', 'Freelancer', 'Front-end Developer', 'Designer'],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
